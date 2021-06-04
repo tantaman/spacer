755042fba5d771db957faad8bb20f830799dce0f
tantaman <matt.crinklaw@gmail.com>
2021-06-04 03:33:01 -0400

    interseting conditionals

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,47 @@
 import "./log.js";
-const game_canvas = document.getElementById("canvas");
-full_screen(game_canvas);
-function full_screen(canvas) { }
+not_null(document.getElementById("canvas"))
+    .then(init_game)
+    .else(() => tell_user(error("Cannot initialize game")));
+function error(msg) { }
+function init_game(canvas) {
+    full_screen(canvas);
+}
+function tell_user(e) { }
+function full_screen(el) {
+    if (el == null) {
+        return;
+    }
+    el.style.width = parseInt(window.innerWidth, 10);
+    el.style.height = parseInt(window.innerHeight, 10);
+}
+function not_null(x) {
+    if (x == null) {
+        return new Fail(null);
+    }
+    return new Pass(x);
+}
+class Pass {
+    p;
+    constructor(p) {
+        this.p = p;
+    }
+    then(cb) {
+        return this;
+    }
+    else(cb) {
+        return this;
+    }
+}
+class Fail {
+    p;
+    constructor(p) {
+        this.p = p;
+    }
+    then(cb) {
+        return this;
+    }
+    else(cb) {
+        return this;
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,58 @@
 import "./log.js";
 
-const game_canvas = document.getElementById("canvas");
+not_null(document.getElementById("canvas"))
+  .then(init_game)
+  .else(() => tell_user(error("Cannot initialize game")));
 
-full_screen(game_canvas);
+function error(msg: string) {}
 
-function full_screen(canvas: HTMLElement) {}
+function init_game(canvas: HTMLCanvasElement) {
+  full_screen(canvas);
+}
+
+function tell_user(e: Error) {}
+
+function full_screen(el: HTMLElement | null) {
+  if (el == null) {
+    return;
+  }
+  el.style.width = parseInt(window.innerWidth, 10);
+  el.style.height = parseInt(window.innerHeight, 10);
+}
+
+function not_null<T>(x: T | null): Condition<T, null> {
+  if (x == null) {
+    return new Fail(null);
+  }
+
+  return new Pass(x);
+}
+
+interface Condition<T, Y> {
+  then(cb: (x: T) => void): this;
+  else(cb: (x: any) => void): this;
+}
+
+class Pass<T> implements Condition<T, any> {
+  constructor(private p: T) {}
+
+  then(cb: (x: T) => void): this {
+    return this;
+  }
+
+  else(cb: (x: any) => void): this {
+    return this;
+  }
+}
+
+class Fail<Y> implements Condition<any, Y> {
+  constructor(private p: Y) {}
+
+  then(cb: (x: T) => void): this {
+    return this;
+  }
+
+  else(cb: (X: Y) => void): this {
+    return this;
+  }
+}
